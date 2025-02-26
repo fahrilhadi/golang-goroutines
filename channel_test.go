439d07589f2acb5f53e06df9332697a8786a9c60
Fahril Hadi <fahrilhadi77@gmail.com>
2025-02-26 21:23:40 +0700

    range channel

--- channel_test.go
+++ channel_test.go
@@ -2,6 +2,7 @@ package golang_goroutines
 
 import (
 	"fmt"
+	"strconv"
 	"testing"
 	"time"
 )
@@ -66,6 +67,23 @@ func TestBufferedChannel(t *testing.T)  {
 	channel <- "Fahril"
 
 	fmt.Println(<- channel)
+
+	fmt.Println("Selesai")
+}
+
+func TestRangeChannel(t *testing.T)  {
+	channel := make(chan string)
+
+	go func ()  {
+		for i := 0; i < 10; i++ {
+			channel <- "Perulangan ke " + strconv.Itoa(i)
+		}
+		close(channel)
+	}()
+
+	for data := range channel {
+		fmt.Println("Menerima data", data)
+	}
 
 	fmt.Println("Selesai")
 }
